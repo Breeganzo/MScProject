6d5712952f84e6b3c894180cea28bd792b066e17
Anthony Breeganzo Thomas <anthonybreeganzo02@gmail.com>
2024-09-30 18:27:51 +0530

    Frontend

---
+++ api/main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("/Users/anthonybreeganzo/IndianPlantLeafDisease/Latest.h5")
+
+CLASS_NAMES = ['Apple___Apple_scab',
+               'Apple___Black_rot',
+               'Apple___Cedar_apple_rust',
+               'Apple___healthy',
+               'Blueberry___healthy',
+               'Cherry_(including_sour)___Powdery_mildew',
+               'Cherry_(including_sour)___healthy',
+               'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+               'Corn_(maize)___Common_rust_',
+               'Corn_(maize)___Northern_Leaf_Blight',
+               'Corn_(maize)___healthy',
+               'Grape___Black_rot',
+               'Grape___Esca_(Black_Measles)',
+               'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+               'Grape___healthy',
+               'Orange___Haunglongbing_(Citrus_greening)',
+               'Peach___Bacterial_spot',
+               'Peach___healthy',
+               'Pepper,_bell___Bacterial_spot',
+               'Pepper,_bell___healthy',
+               'Potato___Early_blight',
+               'Potato___Late_blight',
+               'Potato___healthy',
+               'Raspberry___healthy',
+               'Soybean___healthy',
+               'Squash___Powdery_mildew',
+               'Strawberry___Leaf_scorch',
+               'Strawberry___healthy',
+               'Tomato___Bacterial_spot',
+               'Tomato___Early_blight',
+               'Tomato___Late_blight',
+               'Tomato___Leaf_Mold',
+               'Tomato___Septoria_leaf_spot',
+               'Tomato___Spider_mites Two-spotted_spider_mite',
+               'Tomato___Target_Spot',
+               'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+               'Tomato___Tomato_mosaic_virus',
+               'Tomato___healthy']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = Image.open(BytesIO(data))
+    image = image.resize((256, 256))  # Resize to match model input size
+    image = np.array(image) / 255.0  # Normalize pixel values to [0, 1]
+    if image.shape[-1] == 4:  # If the image has an alpha channel, remove it
+        image = image[..., :3]
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+    
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
